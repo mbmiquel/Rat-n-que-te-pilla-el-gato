2e95f3cfc4e2390627ace5f5b5efbc46448c643b
mbmiquel <mbmiquel@gmail.com>
2016-11-22 21:08:52 +0100

    Add files via upload

---
+++ sesion3_ej14.c
@@ -0,0 +1,16 @@
+#include "colours.h"	
+#include <time.h>
+#include <stdlib.h>	
+#include <stdio.h>
+#include "colours.h"	
+
+main()
+{
+ // Declare aqui las variables necesarias
+
+
+  srand(time(NULL));		// Inicializa semilla del generador de numeros aleatorio de rand
+
+ // Escriba a partir de aqui el codigo del programa
+
+}
